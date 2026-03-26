e063a3993932e6f8956319ca642060b74243e806
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-03-26 12:08:16 -0400

    i made an if statement

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Grace
+ * Created on:March 2026
+ * This program will make a statement ìf
 */
 
-basic.showString('Hello, World!')
+let distance: number = 0
+let neopixelStrip: neopixel.Strip = null
+
+// 
+neopixelStrip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+
+input.onButtonPressed(Button.A, function () {
+    //
+    distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+
+    // 
+    if (distance < 10) {
+        neopixelStrip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else {
+        neopixelStrip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+})
