55508d2b64679a2c62060a1fff3229b669b143b4
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-03-27 13:15:44 -0400

    i made changes in the code

--- main.ts
+++ main.ts
@@ -2,23 +2,37 @@
  *
  * Created by: Grace
  * Created on:March 2026
- * This program will make a statement ìf
+ * This program will make an if... then statement 
 */
 
 let distance: number = 0
-let neopixelStrip: neopixel.Strip = null
+const Strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-// 
-neopixelStrip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+basic.clearScreen()
+basic.showIcon (IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    //
-    distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+ //measuring the distance...
+ basic.clearScreen()
+ distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+    
+    basic.showNumber(distance)
 
-    // 
     if (distance < 10) {
-        neopixelStrip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.showString('< 10 cm')
+        Strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        Strip.show()
     } else {
-        neopixelStrip.showColor(neopixel.colors(NeoPixelColors.Green))
-    }
+        //green if 10cm or further 
+        basic.showString('>= 10 cm')
+        Strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        Strip.show()
+      }
+    })
+
+    input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showIcon(IconNames. Happy)
+Strip.showColor(neopixel.colors(NeoPixelColors.Black))
+Strip.show()
 })
